69e26c2f934c13a3fbf5dcd94c09e2d8c45f6c37
Philippe Roy <philippe@Philippes-MacBook-Pro.local>
2016-08-28 21:47:16 +1000

    Setup

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# broccoliandco
-Airwallex - Frontend Code Challenge
+# Broccoli & Co.
+## Airwallex - Frontend Code Challeng
+
+A simple yet clean homepage for them that allow users to enter their 1.) name 2.) email to receive email invitations.
+
+
+# Installing
+
+1. Clone the repo
+2. `npm install -g gulp` to install Gulp globally.
+3. `npm install` to resolve project dependencies.
+
+# Using
+
+Run `npm start` from the command line and you are good to go!
+
+The project is currently setup to transpile code under the _/src_ folder using the _/src/app.js_ file as an entry point.
+
+The resulting code ends up in the `public` directory.
+
+#Test
+
+Run `npm test` from the command line and watch them all pass along.
+
+The tests can be found _/test_ folder.
---
+++ gulpfile.babel.js
@@ -0,0 +1,88 @@
+import gulp from 'gulp';
+import browserify from 'browserify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+import eslint from 'gulp-eslint';
+import exorcist from 'exorcist';
+import browserSync from 'browser-sync';
+import watchify from 'watchify';
+import babelify from 'babelify';
+import uglify from 'gulp-uglify';
+import ifElse from 'gulp-if-else';
+import mocha from 'gulp-mocha';
+import gutil from 'gulp-util';
+import env from 'gulp-env';
+import sass from 'gulp-sass';
+
+watchify.args.debug = true;
+
+const sync = browserSync.create();
+
+// Input file.
+watchify.args.debug = true;
+var bundler = browserify('src/app.js', watchify.args);
+
+// Babel transform
+bundler.transform(babelify.configure({
+  sourceMapRelative: 'src'
+}));
+
+// On updates recompile
+bundler.on('update', bundle);
+
+function bundle() {
+  return bundler.bundle()
+    .on('error', function(error){
+      console.error( '\nError: ', error.message, '\n');
+      this.emit('end');
+    })
+    .pipe(exorcist('public/assets/js/bundle.js.map'))
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(ifElse(process.env.NODE_ENV === 'production', uglify))
+    .pipe(gulp.dest('public/assets/js'));
+}
+
+gulp.task('default', ['transpile']);
+
+gulp.task('transpile', ['lint'], () => bundle());
+
+gulp.task('lint', () => {
+    return gulp.src(['src/**/*.js', 'gulpfile.babel.js'])
+      .pipe(eslint())
+      .pipe(eslint.format())
+});
+
+gulp.task('serve', ['transpile'], () => sync.init({
+  server: 'public',
+  port: process.env.PORT || 8000,
+  host: process.env.IP || 'localhost'
+}));
+
+gulp.task('js-watch', ['transpile'], () => sync.reload());
+
+gulp.task('watch', ['serve'], () => {
+  gulp.watch('src/**/*', ['js-watch'])
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('public/index.html', sync.reload)
+});
+
+gulp.task('test:watch', () => {
+  gulp.watch(['src/**', 'test/**'], ['mocha']);
+});
+
+gulp.task('mocha', function() {
+    const envs = env.set({ NODE_ENV: 'production' }); // To stop React PropTypes Warnings
+    return gulp.src(['test/*.js'], { read: false })
+        .pipe(envs)
+        .pipe(mocha({ reporter: 'nyan' }))
+        .on('error', gutil.log);
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/assets/css'))
+    .pipe(sync.stream());
+
+});
